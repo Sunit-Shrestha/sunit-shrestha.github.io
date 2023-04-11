67181bbc3330029dfadde3c53850293fce4ae84d
Sunit Shrestha <sunitshrsth@gmail.com>
2023-04-11 14:44:43 +0545

    Config changed.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
