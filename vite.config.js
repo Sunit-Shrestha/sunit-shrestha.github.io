b73479e7db784086440fb538abf974afb11137c0
Sunit Shrestha <sunitshrsth@gmail.com>
2023-04-12 17:18:02 +0545

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
+	build: {
+		outDir: "build",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+			},
+		},
+	},
 });
